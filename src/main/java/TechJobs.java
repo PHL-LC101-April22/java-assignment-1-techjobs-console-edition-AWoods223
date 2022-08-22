7b7b5545409fcaaffc98fe58b71682bc3b62672b
KingYaz <103969614+AWoods223@users.noreply.github.com>
2022-08-22 18:13:46 -0400

    Completed GA 1, tests fail but contents are identical

--- src/main/java/TechJobs.java
+++ src/main/java/TechJobs.java
@@ -119,7 +119,16 @@ public class TechJobs {
 
     // Print a list of jobs
     private static void printJobs(ArrayList<HashMap<String, String>> someJobs) {
-
-        System.out.println("printJobs is not implemented yet");
+        if  (someJobs.size() == 0) {
+            System.out.println("No Results");
+            return;
+        }
+        for(HashMap<String, String> jobs : someJobs) {
+            System.out.println("\n*****");
+            for (Map.Entry<String, String> jobList : jobs.entrySet()) {
+                System.out.println(jobList.getKey() + ": " + jobList.getValue());
+            }
+            System.out.println("*****");
+        }
     }
 }
